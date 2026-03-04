b8bbaa7ebfcad38d88c4cc1a94f86663e3041429
Muhammad Rizki Ghifari <muhrizkighifari@gmail.com>
2026-03-04 16:29:25 +0700

    Add files via upload

---
+++ Assets/Script/Waypoint/Route2PointSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Waypoint/Route 2 Point (Position)", fileName = "Route2PointPos")]
+public class Route2PointPosSO : ScriptableObject
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+}
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Waypoint/TwoPointPositionMover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPointPositionMover : MonoBehaviour
+{
+    public Route2PointPosSO route;
+    public float speed = 2f;
+
+    private Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = route.pointA;
+        target = route.pointB;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position, target,
+            speed * Time.deltaTime
+        );
+
+        if (transform.position == target)
+        {
+            target = (target == route.pointA) ? route.pointB : route.pointA;
+        }
+
+    }
+}
